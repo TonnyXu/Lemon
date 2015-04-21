791fc0f587dd48b4a78dfab8eb1733c67c36a386
Tonny Xu <tonny.xu@gmail.com>
2015-04-21 16:49:18 +0900

    Initial commit with empty project

---
+++ Lemon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Lemon
+//
+//  Created by Tonny Xu on 4/21/15.
+//  Copyright (c) 2015 Tonny Xu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
